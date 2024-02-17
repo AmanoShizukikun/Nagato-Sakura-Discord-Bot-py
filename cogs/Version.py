c7f4c4a693ca581787c1500a68ecca316e4baaab
天野 靜樹 <hackamsk@gmail.com>
2024-02-17 08:16:41 +0800

    Add files via upload

--- cogs/Version.py
+++ cogs/Version.py
@@ -7,13 +7,13 @@ class Version(commands.Cog):
         
     @commands.command(aliases=["VERSION","version","Ver"])
     async def Version(self,ctx):
-        version = "1.5.ν"
+        version = "1.6.0"
         directory = 'assets/preview/'
         output_path = f"{directory}{version}.png"
         await ctx.send(f"# {version}")
         await ctx.send(file=discord.File(output_path))
-        await ctx.send(f"### 重要變更 \n\n - 新增Audio.py、Image.py")
-        await ctx.send(f"### 新增功能 \n\n - !AudioInfo 附上聲音檔 - 查看音樂資訊並顯示波型\n - !AudioReverse 附上聲音檔 - 反轉聲音檔\n - !AudioSpeed [倍速] 附上聲音檔 - 調整聲音速度\n - !AudioBit [位元] 附上聲音檔 - 調整聲音位元\n- !Sharpen [1~100的整數] 附上圖片 - 調整銳化程度\n- !Blur [1~100的整數] 附上圖片 - 調整模糊程度\n- !Mosaic [整數] 附上圖片 - 套上馬賽克效果\n- !Brightness [0~100的整數] 附上圖片 - 調整亮度\n- !Contrast [0~100的整數] 附上圖片 - 調整對比度\n- !Color [0~100的整數] 附上圖片 - 調整飽和度\n- !Icon [透明度(0~100的整數)] 附上圖片 - 添加浮水印")
+        await ctx.send(f"### 重要變更 \n\n - DM.py 及 Tarot.py 新增了/指令現在能更直觀的調用指令了 \n - main_beta.py 新增了 載入指令程式檔案、卸載指令檔案、重新載入程式檔案、載入斜線指令 \n - 精簡了部分程式的行數提高了效率")
+        await ctx.send(f"### 新增功能 \n\n - !FestivalEvent - 當特殊的日子來臨之時會有彩蛋")
         await ctx.send(f"### 已知問題 \n\n - N/A")
     
 async def setup(bot):

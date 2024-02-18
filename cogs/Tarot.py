eac11f77d8f5a161b4b6cd87de4f16497cafa2b5
天野 靜樹 <hackamsk@gmail.com>
2024-02-18 18:17:30 +0800

    Add files via upload

--- cogs/Tarot.py
+++ cogs/Tarot.py
@@ -1,16 +1,17 @@
 import discord
+from discord import app_commands
 from discord.ext import commands
 import random
 import os
 import json
 from PIL import Image
+import tempfile
 
 class Tarot(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(aliases=["tarot","TAROT"])
-    async def Tarot(self, ctx):
+    async def get_tarot_info(self, ctx=None, interaction=None):
         tarot_folder = os.path.abspath('./data/Tarot/')
         os.makedirs(tarot_folder, exist_ok=True)
 
@@ -22,30 +23,46 @@ class Tarot(commands.Cog):
         is_reverse = random.choice([True, False])  
 
         image_filename = os.path.join(tarot_folder, f"{tarot_number}.png")
-
         try:
             with open(image_filename, "rb") as file:
                 picture = discord.File(file)
 
-                tarot_name = tarot_data[str(tarot_number)]["name"]
-                upright_meaning = tarot_data[str(tarot_number)]["upright_position"]
-                reverse_meaning = tarot_data[str(tarot_number)]["reverse_position"]
-                roman_num = tarot_data[str(tarot_number)]["roman_num"]
+                tarot_info = tarot_data[str(tarot_number)]
+                tarot_name = tarot_info["name"]
+                upright_meaning = tarot_info["upright_position"]
+                reverse_meaning = tarot_info["reverse_position"]
+                roman_num = tarot_info["roman_num"]
 
                 if is_reverse:
                     image = Image.open(file)
                     rotated_image = image.rotate(180)
                     rotated_image.save(image_filename)
                     rotated_picture = discord.File(image_filename)
-                    await ctx.send(f"抽到的塔羅牌為 {roman_num}-{tarot_name}（逆位）\n逆位解釋: {reverse_meaning}", file=rotated_picture)
-                    # 恢复图像到正位
+                    if interaction:
+                        await interaction.response.send_message(f"抽到的塔羅牌為 {roman_num}-{tarot_name}（逆位）\n逆位解釋: {reverse_meaning}", file=rotated_picture)
+                    elif ctx:
+                        await ctx.send(f"抽到的塔羅牌為 {roman_num}-{tarot_name}（逆位）\n逆位解釋: {reverse_meaning}", file=rotated_picture)
                     image = rotated_image.rotate(180)
                     image.save(image_filename)
                 else:
-                    await ctx.send(f"抽到的塔羅牌為 {roman_num}-{tarot_name}（正位）\n正位解釋: {upright_meaning}", file=picture)
+                    if interaction:
+                        await interaction.response.send_message(f"抽到的塔羅牌為 {roman_num}-{tarot_name}（正位）\n正位解釋: {upright_meaning}", file=picture)
+                    elif ctx:
+                        await ctx.send(f"抽到的塔羅牌為 {roman_num}-{tarot_name}（正位）\n正位解釋: {upright_meaning}", file=picture)
         except FileNotFoundError:
-            await ctx.send(f"找不到編號為 {tarot_number} 的塔羅牌")
+            if interaction:
+                await interaction.response.send_message(f"找不到編號為 {tarot_number} 的塔羅牌")
+            elif ctx:
+                await ctx.send(f"找不到編號為 {tarot_number} 的塔羅牌")
 
-async def setup(bot):
+    @commands.command(aliases=["tarot","TAROT"])
+    async def Tarot(self, ctx):
+        await self.get_tarot_info(ctx=ctx)
+
+    @app_commands.command(name="tarot", description="!Tarot - 抽塔羅牌")
+    async def tarot(self, interaction: discord.Interaction):
+        await self.get_tarot_info(interaction=interaction)
+
+async def setup(bot: commands.Bot):
     await bot.add_cog(Tarot(bot))
     print("Tarot.py is ready")
--- cogs/Version.py
+++ cogs/Version.py
@@ -1,21 +1,31 @@
 import discord
+from discord import app_commands
 from discord.ext import commands
+
+def create_version_message(version):
+    version_message = f"# {version}\n"
+    version_message += f"### 重要變更 \n\n - DM.py 及 Tarot.py 新增了/指令現在能更直觀的調用指令了 \n - main_beta.py 新增了 載入指令程式檔案、卸載指令檔案、重新載入程式檔案、載入斜線指令 \n - 精簡了部分程式的行數提高了效率\n"
+    version_message += f"### 新增功能 \n\n - !FestivalEvent - 當特殊的日子來臨之時會有彩蛋\n"
+    version_message += f"### 已知問題 \n\n - N/A"
+    return version_message
 
 class Version(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.directory = 'assets/preview/'
+        self.version = "1.6.0"
+        self.output_path = f"{self.directory}{self.version}.png"
         
     @commands.command(aliases=["VERSION","version","Ver"])
-    async def Version(self,ctx):
-        version = "1.6.0"
-        directory = 'assets/preview/'
-        output_path = f"{directory}{version}.png"
-        await ctx.send(f"# {version}")
-        await ctx.send(file=discord.File(output_path))
-        await ctx.send(f"### 重要變更 \n\n - DM.py 及 Tarot.py 新增了/指令現在能更直觀的調用指令了 \n - main_beta.py 新增了 載入指令程式檔案、卸載指令檔案、重新載入程式檔案、載入斜線指令 \n - 精簡了部分程式的行數提高了效率")
-        await ctx.send(f"### 新增功能 \n\n - !FestivalEvent - 當特殊的日子來臨之時會有彩蛋")
-        await ctx.send(f"### 已知問題 \n\n - N/A")
+    async def Version(self, ctx):
+        version_message = create_version_message(self.version)
+        await ctx.send(version_message, file=discord.File(self.output_path))
+        
+    @app_commands.command(name="version", description="!Version - 顯示長門櫻當前版本")
+    async def version(self, interaction: discord.Interaction):
+        version_message = create_version_message(self.version)
+        await interaction.response.send_message(version_message, file=discord.File(self.output_path))
     
-async def setup(bot):
+async def setup(bot: commands.Bot):
     await bot.add_cog(Version(bot))
     print("Version.py is ready")
